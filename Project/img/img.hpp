a40523519600b82a80e1748150ea141e689a314d
chrjor <90344922+chrjor@users.noreply.github.com>
2023-05-29 09:55:57 -0500

    updated img

--- Project/img/img.hpp
+++ Project/img/img.hpp
@@ -12,6 +12,7 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <variant>
 
 #include <boost/gil.hpp>
 #include <boost/gil/extension/io/jpeg.hpp>
@@ -33,27 +34,37 @@ using boost::gil::png_tag;
 using boost::gil::bmp_tag;
 using boost::gil::rgb8_image_t;
 
+// Enum for image tag type
+enum class ImageTagType {
+    JPEG,
+    PNG,
+    BMP
+};
 
 // Image classes for concrete image factory
 class Image : public Visitable<Image> {
 public:
-    Image(std::string_view path) {
-        if (auto [whole, out, file, ext] = ctre::match<"(.*)(\\.(jpg|png|jpeg|bmp))">(path); whole) {
-            
-            if (!out.to_view().empty() && !file.to_view().empty() && !ext.to_view().empty()) {
-                img = make_unique<rgb8_image_t>();
+    Image(std::string_view path) : tag() {
+        if (auto [whole, pre, dir, post, ext] = ctre::match<"(.*)(\\/in\\/)(.*)(jpg|png|jpeg|bmp)">(path); whole) {
 
+            if (!pre.to_view().empty() && !dir.to_view().empty()
+                && !post.to_view().empty() && !ext.to_view().empty()
+                ) {
+                img = make_unique<rgb8_image_t>();
                 if (ext.to_view() == "jpg" || ext.to_view() == "jpeg") {
                     read_image(std::string(path), *img, jpeg_tag{});
+                    tag = ImageTagType::JPEG;
                 }
                 else if (ext.to_view() == "png") {
                     read_image(std::string(path), *img, png_tag{});
+                    tag = ImageTagType::PNG;
                 }
                 else if (ext.to_view() == "bmp") {
                     read_image(std::string(path), *img, bmp_tag{});
+                    tag = ImageTagType::BMP;
                 }
-                out_dir = out.to_string();
-                file_name = file.to_string();
+
+                out_path = pre.to_string() + "/out/" + post.to_string() +  ext.to_string();
             }
             else {
                 throw std::runtime_error("Invalid file path");
@@ -65,34 +76,40 @@ public:
 
     Image(const Image& other) {
         img = make_unique<rgb8_image_t>(*other.img);
-        out_dir = other.out_dir;
-        file_name = other.file_name;
+        out_path = other.out_path;
     }
 
     Image& operator=(const Image& other) {
         img = make_unique<rgb8_image_t>(*other.img);
-        out_dir = other.out_dir;
-        file_name = other.file_name;
+        out_path = other.out_path;
         return *this;
     }
 
     Image(Image&& other) noexcept {
         img = std::move(other.img);
-        out_dir = other.out_dir;
-        file_name = other.file_name;
+        out_path = other.out_path;
     }
 
     Image& operator=(Image&& other) noexcept {
         img = std::move(other.img);
-        out_dir = other.out_dir;
-        file_name = other.file_name;
+        out_path = other.out_path;
         return *this;
     }
     
     template<typename Visitor>
     void accept(Visitor& v) { v.visit(*this); }
 
-    void write_out() { write_view(out_dir + "_out" + file_name, view(*img), jpeg_tag{}); }
+    void write_out() { 
+        if (tag == ImageTagType::JPEG) {
+            write_view(out_path, const_view(*img), jpeg_tag{});
+        }
+        else if (tag == ImageTagType::PNG) {
+            write_view(out_path, const_view(*img), png_tag{});
+        }
+        else if (tag == ImageTagType::BMP) {
+            write_view(out_path, const_view(*img), bmp_tag{});
+        }
+    }
 
     rgb8_image_t& get_image() { return *img; }
 
@@ -102,8 +119,8 @@ public:
 
 private:
     unique_ptr<rgb8_image_t> img;
-    string out_dir;
-    string file_name;
+    ImageTagType tag;
+    string out_path;
 };
 
 #endif
